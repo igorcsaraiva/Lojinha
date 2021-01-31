a43836c533623b1b08c2c04fc74e9d80a9681fdf
Igor Correa <igor.correa@viannasempre.com.br>
2021-01-31 14:43:13 -0300

    Inicio da implementacao da view de pedidos

---
+++ Loja/Loja.Application/ViewModels/PedidoViewModelCriar.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace Loja.Application.ViewModels
+{
+    public class PedidoViewModelCriar
+    {
+        [Required(ErrorMessage ="Selecione um cliente")]
+        public int ClienteID { get; set; }
+
+        [Required]
+        public int[] ProdutosID { get; set; }
+       
+        [Required]
+        public int[] Quantidade { get; set; }
+    }
+}
---
+++ Loja/Loja.Web/Controllers/PedidoController.cs
@@ -0,0 +1,112 @@
+﻿using Loja.Application.Interfaces;
+using Loja.Application.ViewModels;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Loja.Web.Controllers
+{
+    public class PedidoController : Controller
+    {
+        private readonly IClienteAppSevicos _clienteAppSevicos;
+        private readonly IProdutoAppServico _produtoAppServico;
+        public PedidoController(IClienteAppSevicos clienteAppSevicos, IProdutoAppServico produtoAppServico)
+        {
+            _clienteAppSevicos = clienteAppSevicos;
+            _produtoAppServico = produtoAppServico;
+        }
+        // GET: PedidoController
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: PedidoController/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: PedidoController/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: PedidoController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(PedidoViewModelCriar produtoViewModelCriar)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: PedidoController/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: PedidoController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: PedidoController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: PedidoController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        [HttpGet]
+        public JsonResult CarregarClientes()
+        {
+            var ListaCliente = _clienteAppSevicos.BuscarTodos().Result;
+
+            return Json(new { ListaCliente });
+        }
+
+        [HttpGet]
+        public JsonResult CarregarProdutos()
+        {
+            var ListaProduto = _produtoAppServico.BuscarTodos().Result;
+
+            return Json(new { ListaProduto });
+        }
+    }
+}
